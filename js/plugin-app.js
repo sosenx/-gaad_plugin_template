3eb306adf105deaa8f76dba171a2f3d2a4702355
sosenx <barteksosnowski711@gmail.com>
2018-01-01 21:51:35 +0100

    fixed some id names

--- js/plugin-app.js
+++ js/plugin-app.js
@@ -1,39 +1,37 @@
 (function(window, Vue, VueRouter){
 
-	// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
-	// and then call `Vue.use(VueRouter)`.
 
-	// 1. Define route components.
-	// These can be imported from other files
 	const Base = { template: '#template-plugins_main_namespace-main' }
 	const Foo = { template: '<div>foo</div>' }
 	const Bar = { template: '<div>bar</div>' }
 
-	// 2. Define some routes
-	// Each route should map to a component. The "component" can
-	// either be an actual component constructor created via
-	// `Vue.extend()`, or just a component options object.
-	// We'll talk about nested routes later.
+	
 	const routes = [
 	  { path: '/', component: Base },
 	  { path: '/foo', component: Foo },
 	  { path: '/bar', component: Bar }
 	]
 
-	// 3. Create the router instance and pass the `routes` option
-	// You can pass in additional options here, but let's
-	// keep it simple for now.
-	const router = new VueRouter({
-	  routes, // short for `routes: routes`
-	  base : '/'
+
+	const store = new Vuex.Store({
+	  state: {
+	    count: 0
+	  },
+	  mutations: {
+	    increment (state) {
+	      state.count++
+	    }
+	  }
 	})
 
-	// 4. Create and mount the root instance.
-	// Make sure to inject the router with the router option to make the
-	// whole app router-aware.
+	const router = new VueRouter({
+	  routes
+	})
+
+	
 	const app = new Vue({
+	  store,
 	  router
 	}).$mount('#app-plugins_main_namespace');
-// Now the app has started!
 
 })(window, Vue, VueRouter);	
